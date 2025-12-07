0aada788bbb1ed100b1fc3cba791eb362b08ec61
Diego Alvarez S. <dialvarezs@gmail.com>
2025-12-06 21:06:16 -0300

    feat(deploy): Add deployment with Podman

---
+++ deploy/build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+tag_prefix="app"
+date=$(date '+%Y%m%d')
+
+build_image() {
+  local target=$1
+  local dir=$2
+  podman build --pull=newer -t "${tag_prefix}_${target}:${date}" -t "${tag_prefix}_${target}:latest" "${dir}/"
+}
+
+build_target=${1:-all}
+
+case $build_target in
+  api)
+    build_image "api" ../app_api/
+    ;;
+  web)
+    build_image "web" ../app_web/
+    ;;
+  all)
+    build_image "api" ../app_api/
+    build_image "web" ../app_web/
+    ;;
+  *)
+    echo "Uso: $0 [api|web|all]"
+    exit 1
+    ;;
+esac
